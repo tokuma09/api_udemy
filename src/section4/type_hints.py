1022816ccd9e6b190ae044b072899c8075f4ac8e
Tokuma Suzuki <tokuma.suzuki@jdsc.ai>
2024-10-03 20:35:59 +0900

    add linter

--- src/section4/type_hints.py
+++ src/section4/type_hints.py
@@ -5,12 +5,10 @@ def calc_price_including_tax(price: int, tax: float) -> int:
     return int(price * tax)
 
 
-
-
-if __name__ == '__main__':
-    price:int = 100
-    tax:float = 1.1
+if __name__ == "__main__":
+    price: int = 100
+    tax: float = 1.1
     print(f"{calc_price_including_tax(price=price, tax=tax)}円")
 
-    sample_list: List[int] = [1,2, 3,4]
-    sample_dict: Dict[str, str] = {'user_name':'abcd'}
+    sample_list: List[int] = [1, 2, 3, 4]
+    sample_dict: Dict[str, str] = {"user_name": "abcd"}
