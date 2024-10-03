b6b92aba172987786c98382ee266b78352a1fadb
Tokuma Suzuki <tokuma.suzuki@jdsc.ai>
2024-10-03 20:31:00 +0900

    add first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# api-udemy
+
+Describe your project here.
---
+++ src/section4/type_hints.py
@@ -0,0 +1,16 @@
+from typing import Dict, List
+
+
+def calc_price_including_tax(price: int, tax: float) -> int:
+    return int(price * tax)
+
+
+
+
+if __name__ == '__main__':
+    price:int = 100
+    tax:float = 1.1
+    print(f"{calc_price_including_tax(price=price, tax=tax)}円")
+
+    sample_list: List[int] = [1,2, 3,4]
+    sample_dict: Dict[str, str] = {'user_name':'abcd'}
